e05bea04662a1256880313a616ae5455be23ec95
Tsvetan Marinov <lamarinov@gmail.com>
2017-04-30 00:38:06 +0300

    Add project

---
+++ Blog-Skeleton/Blog.UI.Tests/UITests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.UI.Tests
+{
+    [TestFixture]
+    public class UITests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}

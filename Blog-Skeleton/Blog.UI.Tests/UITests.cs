c5e52a671b06e528baa0d6e72ede481d178a9d68
Tsvetan Marinov <lamarinov@gmail.com>
2017-05-19 15:52:41 +0300

    Configuring browser host for seleno

--- Blog-Skeleton/Blog.UI.Tests/UITests.cs
+++ Blog-Skeleton/Blog.UI.Tests/UITests.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +13,18 @@ namespace Blog.UI.Tests
     public class UITests
     {
         [Test]
-        public void TestMethod()
+        public void CheckSiteLoad()
         {
-            // TODO: Add your test code here
-            Assert.Pass("Your first passing test");
+            IWebDriver driver = BrowserHost.Instance.Application.Browser;
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+
+
+            driver.Navigate().GoToUrl(@"http://localhost:60634/Article/List");
+
+            var logo = wait.Until(w => w.FindElement(By.XPath("/html/body/div[1]/div/div[1]/a")));
+
+            Assert.AreEqual("SOFTUNI BLOG", logo.Text);
         }
     }
 }
